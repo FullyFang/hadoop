8080f362f3e37d49b66c5a22d12faa162758402d
robbyzhang <robby@hadoop.main>
2012-09-24 20:15:16 +0800

    1

---
+++ weibo/src/org/robby/Post.java
@@ -0,0 +1,13 @@
+package org.robby;
+
+public class Post {
+	public String username;
+	public String content;
+	public String time;
+	
+	public Post(String username, String content, String time){
+		this.username = username;
+		this.content = content;
+		this.time = time;
+	}
+}
