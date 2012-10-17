5632ae89fd0f3d60a41f2587672d46feebe392ca
robbyzhang <icw_zy@163.com>
2012-10-17 17:24:12 +0800

    1

---
+++ hive/hive.sql
@@ -0,0 +1,7 @@
+create table tab_cdr(oaddr string, oareacode string, daddr string, dareacode string, ts string, type string) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' STORED AS TEXTFILE LOCATION '/cdr00.txt';
+
+create table tab_res(name string, num int);
+
+insert overwrite table tab_res select 'tab_cdr', count(*) from tab_cdr;
+
+insert into table tab_res select 'tab_cdr', count(*) from tab_cdr where type='0';
