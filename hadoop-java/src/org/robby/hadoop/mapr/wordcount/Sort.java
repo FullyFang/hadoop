3a977f3ec51f318d9bde5e93b053caccbe4c3cc0
robbyzhang <robby@hadoop.main>
2012-11-11 18:44:02 +0800

    1

---
+++ hadoop-java/src/org/robby/hadoop/mapr/wordcount/Sort.java
@@ -0,0 +1,61 @@
+
+
+import java.io.IOException;
+import java.util.*;
+import java.util.Map.Entry;
+
+
+import org.apache.hadoop.fs.Path;
+
+import org.apache.hadoop.io.*;
+
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import org.apache.hadoop.util.*;
+
+public class Sort{
+	
+   public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+     private final static IntWritable one = new IntWritable(1);
+     private Text word = new Text();
+
+     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+       String line = value.toString();
+       String[] arr = line.split(",");
+       
+       word.set(arr[0]);
+       context.write(word, one);
+       
+     }
+   }
+
+   public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+     public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+    	int i=0;
+    	for (IntWritable value : values){
+    		i++;
+    	}
+ 		context.write(key, new IntWritable(i));
+     }
+   }
+
+   public static void main(String[] args) throws Exception {
+	   	Job job = new Job();
+		job.setJarByClass(Sort.class);
+		job.setJobName("Sort");
+
+		FileInputFormat.addInputPath(job, new Path("hdfs://localhost:9000/input/"));
+		FileOutputFormat.setOutputPath(job, new Path("hdfs://localhost:9000/output/"));
+		job.setMapperClass(Map.class);
+		//job.setCombinerClass(Reduce.class);
+		job.setReducerClass(Reduce.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		job.setNumReduceTasks(2);
+
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+   }
+}
---
+++ hadoop-java/src/org/robby/hadoop/mapr/wordcount/Sort3.java
@@ -0,0 +1,75 @@
+
+
+import java.io.IOException;
+import java.util.*;
+import java.util.Map.Entry;
+
+
+import org.apache.hadoop.fs.Path;
+
+import org.apache.hadoop.io.*;
+
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.partition.TotalOrderPartitioner;
+
+import org.apache.hadoop.util.*;
+
+public class Sort3{
+	public static class TestPar extends Partitioner<Text, IntWritable>{
+		@Override
+		public int getPartition(Text arg0, IntWritable arg1, int arg2) {
+			// TODO Auto-generated method stub
+			if(Integer.parseInt(arg0.toString().substring(7)) < 500)
+				return 0;
+			else
+				return 1;
+		}
+	}
+	
+   public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+     private final static IntWritable one = new IntWritable(1);
+     private Text word = new Text();
+
+     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+       String line = value.toString();
+       String[] arr = line.split(",");
+       
+       word.set(arr[0]);
+       context.write(word, one);
+       
+     }
+   }
+
+   public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+     public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+    	int i=0;
+    	for (IntWritable value : values){
+    		i++;
+    	}
+ 		context.write(key, new IntWritable(i));
+     }
+   }
+
+   public static void main(String[] args) throws Exception {
+	   	Job job = new Job();
+		job.setJarByClass(Sort3.class);
+		job.setJobName("Sort2");
+
+		FileInputFormat.addInputPath(job, new Path("hdfs://localhost:9000/input/"));
+		FileOutputFormat.setOutputPath(job, new Path("hdfs://localhost:9000/output/"));
+		job.setMapperClass(Map.class);
+		//job.setCombinerClass(Reduce.class);
+		job.setReducerClass(Reduce.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		job.setNumReduceTasks(3);
+		
+		
+		job.setPartitionerClass(TestPar.class);
+		
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+   }
+}
